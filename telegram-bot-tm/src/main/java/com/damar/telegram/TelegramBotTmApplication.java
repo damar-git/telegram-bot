4399858034197e4a6317b1cf157d51a0c72d46c8
damar-git <davide.rapacciuolo@gmail.com>
2020-05-03 23:40:09 +0200

    Working version

---
+++ telegram-bot-tm/src/main/java/com/damar/telegram/TelegramBotTmApplication.java
@@ -0,0 +1,13 @@
+package com.damar.telegram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotTmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramBotTmApplication.class, args);
+	}
+
+}
---
+++ telegram-bot-tm/src/test/java/com/damar/telegram/TelegramBotTmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damar.telegram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotTmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
